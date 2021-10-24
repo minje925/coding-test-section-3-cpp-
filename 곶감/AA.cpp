1e1ae7508e0fe7d270798ad7b7ec4f1fd9afd138
minje925 <52901503+minje925@users.noreply.github.com>
2021-10-24 16:47:30 +0900

    Update AA.cpp

--- 곶감/AA.cpp
+++ 곶감/AA.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
 
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			int temp;
 			cin >> temp;
 			tv.push_back(temp);
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
 	}
 	cin >> m;
 	for (int i = 0; i < m; i++) {
-		for (int j = 0; i < m; i++) {
+		for (int j = 0; j < 3; j++) {
 			int temp;
 			cin >> temp;
 			tv.push_back(temp);
@@ -34,29 +34,48 @@ int main(int argc, char* argv[]) {
 		q.push_back(tv);
 		tv.clear();
 	}
-
+	cout<<"qsize : "<<q.size()<<endl;
 	for (int i = 0; i < q.size(); i++) {
 		if (q[i][1] == 0) {
-			// ¿ÞÂÊ
+			// Â¿ÃžÃ‚ÃŠ
 			for (int j = 0; j < q[i][2]; j++) {
-				int temp = v[q[i][0]].front();
-				v[q[i][0]].erase(v[q[i][0]].begin());
-				v[q[i][0]].push_back(temp);
+				int temp = v[q[i][0]-1].front();
+				v[q[i][0]-1].erase(v[q[i][0]-1].begin());
+				v[q[i][0]-1].push_back(temp);
 			}
+		}
+		else if(q[i][1] == 1) {
+			for (int j = 0; j < q[i][2]; j++) {
+				int temp = v[q[i][0]-1].back();
+				v[q[i][0]-1].pop_back();
+				v[q[i][0]-1].insert(v[q[i][0]-1].begin(), temp);
+			}
+			
+		}
+	}
+	int sum = 0;
+	int s, e = n;
+	s = 0;
+	int mid = n/2;
+	for (int i = 0; i < n; i++) {
+		if(mid > i) {
+			for (int j= s; j < e; j++) {
+				cout<<v[i][j]<<" ";
+				sum+=v[i][j];
+			}
+			cout<<endl;
+			s++;
+			e--;
 		}
 		else {
-			// ¿À¸¥ÂÊ
-			for (int j = 0; j < q[i][2]; j++) {
-				int temp = v[q[i][0]].back();
-				v[q[i][0]].pop_back();
-				v[q[i][0]].insert(v[q[i][0]].begin(), temp);
+			for (int j= s; j < e; j++) {
+				cout<<v[i][j]<<" ";
+				sum+=v[i][j];
 			}
+			cout<<endl;
+			s--;
+			e++;
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; i < n; i++) {
-			cout << v[i][j] << " ";
-		}
-		cout << endl;
-	}
+	cout<<sum<<endl;
 }
